de036981d21897747a2ec8ef29274177a497158f
Voriah <rodgick@yahoo.com>
2018-12-18 18:56:36 -0800

    Updates

---
+++ precache-manifest.6f6b6c3a7d71a7535d5e3e231fa71793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hotjelly/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "76d1d4b195f480784725",
+    "url": "/hotjelly/static/js/runtime~main.76d1d4b1.js"
+  },
+  {
+    "revision": "947c4d65fa9a297a60bb",
+    "url": "/hotjelly/static/js/main.947c4d65.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/hotjelly/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "947c4d65fa9a297a60bb",
+    "url": "/hotjelly/static/css/main.049a18bc.chunk.css"
+  },
+  {
+    "revision": "231f73c694c1bef538443af10756cae2",
+    "url": "/hotjelly/index.html"
+  }
+];
